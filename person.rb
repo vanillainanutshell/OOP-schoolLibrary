543de4530b886b89d780fa3da0a2189b547f8a67
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-05-11 18:04:54 -0300

    Linters

--- person.rb
+++ person.rb
@@ -2,17 +2,16 @@ require_relative 'nameable'
 require_relative 'rental'
 
 class Person < Nameable
-  attr_reader :id,
   attr_accessor :age, :rentals, :name
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-    @rentals = []
-  end
+  attr_reader :id,
+              def initialize(age, name = 'Unknown', parent_permission: true)
+                super()
+                @id = Random.rand(1..1000)
+                @name = name
+                @age = age
+                @parent_permission = parent_permission
+                @rentals = []
+              end
 
   def can_use_services?
     @parent_permission || of_age?
